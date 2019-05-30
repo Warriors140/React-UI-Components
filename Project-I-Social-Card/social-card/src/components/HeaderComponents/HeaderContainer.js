5f965c3245f861a67c1b0a0aa07b88ed4671195b
Randy Warrington <warrington.randy@outlook.com>
2019-05-29 21:35:27 -0400

    completed project

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+import Tittle from './HeaderTitle';
+import Content from './HeaderContent';
+import Thumbnail from './ImageThumbnail';
+
+
+function HeaderContainer () {
+    return (<div className= 'style'>
+        <Thumbnail />
+        <div className= 'header'>
+        <Tittle />
+        <Content /> 
+        </div>
+        
+    </div>
+    )
+}
+
+export default HeaderContainer
